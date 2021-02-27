75f5d577ee02d27dae227610d0fe2fff3c39108b
Kiran Kumar <kiran.kumar3@royalmail.com>
2021-02-27 10:33:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START gae_python38_app]
+# [START gae_python3_app]
+from flask import Flask, render_template
+
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """Return index.html"""
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    """Return about.html"""
+    return render_template('about.html')
+
+@app.route('/services')
+def services():
+    """Return services.html"""
+    return render_template('services.html')
+
+@app.route('/careers')
+def careers():
+    """Return careers.html"""
+    return render_template('careers.html')
+
+@app.route('/contact')
+def contact():
+    """Return contact.html"""
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_python3_app]
+# [END gae_python38_app]
